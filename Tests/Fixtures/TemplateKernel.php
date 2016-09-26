9788446f0d6fc32051a2eccd61bc1c793b3c055c
Jérémy DECOOL <contact@jdecool.fr>
2016-09-26 21:00:36 +0200

    Functional test for Twig extension

---
+++ Tests/Fixtures/TemplateKernel.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Novaway\Bundle\FeatureFlagBundle\Tests\Fixtures;
+
+use Novaway\Bundle\FeatureFlagBundle\NovawayFeatureFlagBundle;
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Bundle\TwigBundle\TwigBundle;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpKernel\Kernel;
+
+class TemplateKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        return [
+            new FrameworkBundle(),
+            new TwigBundle(),
+            new NovawayFeatureFlagBundle(),
+        ];
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(function ($container) {
+            $container->loadFromExtension('framework', [
+                'secret' => '$ecret',
+            ]);
+
+            $container->loadFromExtension('novaway_feature_flag', [
+                'features' => [
+                    'foo' => true,
+                    'bar' => false,
+                ],
+            ]);
+        });
+    }
+
+    public function getCacheDir()
+    {
+        return sprintf('%s/logs/cache/%s', $this->getBasePath(), $this->environment);
+    }
+
+    public function getLogDir()
+    {
+        return sprintf('%s/logs', $this->getBasePath());
+    }
+
+    public function getBasePath()
+    {
+        return sprintf('%s/%s/TemplateKernel', sys_get_temp_dir(), Kernel::VERSION);
+    }
+}
---
+++ Tests/Functionals/Fixtures/TemplateKernel.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Novaway\Bundle\FeatureFlagBundle\Tests\Functionals\Fixtures;
+
+use atoum;
+use Novaway\Bundle\FeatureFlagBundle\Tests\Fixtures\TemplateKernel as TestedClass;
+use Symfony\Component\Filesystem\Filesystem;
+
+/**
+ * @namespace Functionals
+ * @engine inline
+ */
+class TemplateKernel extends atoum
+{
+    private $kernel;
+
+    public function setUp()
+    {
+        $this->kernel = new TestedClass('dev', true);
+        if (file_exists($this->kernel->getBasePath())) {
+            $fs = new Filesystem();
+            $fs->remove($this->kernel->getBasePath());
+        }
+
+        $this->kernel->boot();
+    }
+
+    public function tearDown()
+    {
+        $fs = new Filesystem();
+        $fs->remove($this->kernel->getBasePath());
+    }
+
+    public function testFeatureEnable()
+    {
+        $this
+            ->if($this->kernel)
+            ->then
+                ->string($this->kernel->getContainer()->get('twig')->render('index.html.twig'))
+                    ->contains('Foo is activate')
+                    ->notContains('Bar is activate')
+        ;
+    }
+
+    public function testFeatureDisable()
+    {
+        $this
+            ->if($this->kernel)
+            ->then
+                ->string($this->kernel->getContainer()->get('twig')->render('index.html.twig'))
+                    ->contains('Bar is disable')
+                    ->notContains('Foo is disable')
+        ;
+    }
+}
