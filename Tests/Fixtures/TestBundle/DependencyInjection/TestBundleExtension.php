e2e518b43ce1829d0ffdb8251d5f3c4af7cc31eb
Jérémy DECOOL <contact@jdecool.fr>
2022-05-24 20:44:53 +0200

    Migrate configuration to YAML format instead of PHP format (SF 4.4 compatibility)

--- Tests/Fixtures/TestBundle/DependencyInjection/TestBundleExtension.php
+++ Tests/Fixtures/TestBundle/DependencyInjection/TestBundleExtension.php
@@ -6,17 +6,17 @@ namespace Novaway\Bundle\FeatureFlagBundle\Tests\Fixtures\TestBundle\DependencyI
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class TestBundleExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new PhpFileLoader(
+        $loader = new YamlFileLoader(
             $container,
             new FileLocator(__DIR__.'/../Resources/config')
         );
 
-        $loader->load('services.php');
+        $loader->load('services.yaml');
     }
 }
