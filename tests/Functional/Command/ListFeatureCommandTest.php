76a94863bb95b5c596d71c1a2ec3764cbac30460
Jérémy DECOOL <contact@jdecool.fr>
2023-05-01 15:44:27 +0200

    Register ListFeatureCommand in dependency container

---
+++ tests/Functional/Command/ListFeatureCommandTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the NovawayFeatureFlagBundle package.
+ * (c) Novaway <https://github.com/novaway/NovawayFeatureFlagBundle>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Novaway\Bundle\FeatureFlagBundle\Tests\Functional\Command;
+
+use Novaway\Bundle\FeatureFlagBundle\Tests\Functional\WebTestCase;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Command\Command;
+
+final class ListFeatureCommandTest extends WebTestCase
+{
+    public function testCommandIsAvailable(): void
+    {
+        $application = new Application(static::bootKernel());
+        $command = $application->find('novaway:feature-flag:list');
+
+        static::assertInstanceOf(Command::class, $command);
+    }
+}
