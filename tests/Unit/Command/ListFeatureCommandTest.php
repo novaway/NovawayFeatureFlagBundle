3065f67de4b3b8eb162fbb4c52085bcc74f8a80a
Jérémy DECOOL <contact@jdecool.fr>
2023-05-01 14:56:59 +0200

    Mutualiaze ListFeatureCommand test case

--- tests/Unit/Command/ListFeatureCommandTest.php
+++ tests/Unit/Command/ListFeatureCommandTest.php
@@ -18,59 +18,28 @@ use Symfony\Component\Console\Tester\CommandTester;
 
 final class ListFeatureCommandTest extends TestCase
 {
-    /**
-     * @dataProvider tableListFeatureProvider
-     */
-    public function testConfiguredFeaturesAreDisplayedInTable(array $features, string $expectedOutput): void
-    {
-        $commandTester = $this->createCommandTester($features);
-        $commandTester->execute([]);
-
-        $commandTester->assertCommandIsSuccessful();
-
-        static::assertSame($expectedOutput, $commandTester->getDisplay());
-    }
-
-    /**
-     * @dataProvider jsonListFeatureProvider
-     */
-    public function testConfiguredFeaturesAreDisplayedInJson(array $features, string $expectedOutput): void
-    {
-        $commandTester = $this->createCommandTester($features);
-        $commandTester->execute(['--format' => 'json']);
-
-        $commandTester->assertCommandIsSuccessful();
-
-        static::assertSame($expectedOutput, $commandTester->getDisplay());
-    }
-
-    /**
-     * @dataProvider csvListFeatureProvider
-     */
-    public function testConfiguredFeaturesAreDisplayedInCsv(array $features, string $expectedOutput): void
-    {
-        $commandTester = $this->createCommandTester($features);
-        $commandTester->execute(['--format' => 'csv']);
-
-        $commandTester->assertCommandIsSuccessful();
-
-        static::assertSame($expectedOutput, $commandTester->getDisplay());
-    }
-
-    public function tableListFeatureProvider(): iterable
-    {
-        yield 'no feature' => [
-            [],
-            <<<OUTPUT
+    private const TEST_DATA = [
+        'empty-features' => [
+            'features' => [],
+            'output' => [
+                'table' => <<<OUTPUT
 +------+---------+-------------+
 | Name | Enabled | Description |
 +------+---------+-------------+
 
-OUTPUT
-        ];
+OUTPUT,
+                'json' => <<<JSON
+[]
 
-        yield 'with features' => [
-            [
+JSON,
+                'csv' => <<<CSV
+Name,Enabled,Description
+
+CSV,
+            ],
+        ],
+        'with-features' => [
+            'features' => [
                 'feature1' => [
                     'enabled' => true,
                     'description' => 'Feature 1 description',
@@ -84,7 +53,8 @@ OUTPUT
                     'description' => 'Feature 3 description',
                 ],
             ],
-            <<<OUTPUT
+            'output' => [
+                'table' => <<<OUTPUT
 +----------+---------+-----------------------+
 | Name     | Enabled | Description           |
 +----------+---------+-----------------------+
@@ -93,36 +63,8 @@ OUTPUT
 | feature3 | Yes     | Feature 3 description |
 +----------+---------+-----------------------+
 
-OUTPUT
-        ];
-    }
-
-    public function jsonListFeatureProvider(): iterable
-    {
-        yield 'no feature' => [
-            [],
-            <<<JSON
-[]
-
-JSON
-        ];
-
-        yield 'with features' => [
-            [
-                'feature1' => [
-                    'enabled' => true,
-                    'description' => 'Feature 1 description',
-                ],
-                'feature2' => [
-                    'enabled' => false,
-                    'description' => 'Feature 2 description',
-                ],
-                'feature3' => [
-                    'enabled' => true,
-                    'description' => 'Feature 3 description',
-                ],
-            ],
-            <<<JSON
+OUTPUT,
+                'json' => <<<JSON
 {
     "feature1": {
         "key": "feature1",
@@ -141,43 +83,47 @@ JSON
     }
 }
 
-JSON
-        ];
-    }
-
-    public function csvListFeatureProvider(): iterable
-    {
-        yield 'no feature' => [
-            [],
-            <<<CSV
-Name,Enabled,Description
-
-CSV
-        ];
-
-        yield 'with features' => [
-            [
-                'feature1' => [
-                    'enabled' => true,
-                    'description' => 'Feature 1 description',
-                ],
-                'feature2' => [
-                    'enabled' => false,
-                    'description' => 'Feature 2 description',
-                ],
-                'feature3' => [
-                    'enabled' => true,
-                    'description' => 'Feature 3 description',
-                ],
-            ],
-            <<<CSV
+JSON,
+                'csv' => <<<CSV
 Name,Enabled,Description
 feature1,1,"Feature 1 description"
 feature2,,"Feature 2 description"
 feature3,1,"Feature 3 description"
 
-CSV
-        ];
+CSV,
+            ],
+        ],
+    ];
+
+    public function testAnErrorOccuredIfInvalidFormatIsProvided(): void
+    {
+        $commandTester = $this->createCommandTester();
+
+        $this->expectException(\InvalidArgumentException::class);
+
+        $commandTester->execute(['--format' => 'invalid']);
+    }
+
+    /**
+     * @dataProvider featuresProvider
+     */
+    public function testConfiguredFeaturesAreDisplayedInAskedFormat(array $features, string $outputFormat, string $expectedOutput): void
+    {
+        $commandTester = $this->createCommandTester($features);
+        $commandTester->execute(['--format' => $outputFormat]);
+
+        $commandTester->assertCommandIsSuccessful();
+
+        static::assertSame($expectedOutput, $commandTester->getDisplay());
+    }
+
+    private function featuresProvider(): iterable
+    {
+        foreach (self::TEST_DATA as $caseDescription => $testData) {
+            foreach ($testData['output'] as $format => $expectedOutput) {
+                yield "$caseDescription in $format format" => [$testData['features'], $format, $expectedOutput];
+            }
+        }
     }
 
     private function createCommandTester(array $features = []): CommandTester
